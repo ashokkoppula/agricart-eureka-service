5d56ba0e02a39492baf7e455f4d6d2cb98edbccc
ashokkoppula <thisisashokkumarreddy@gmail.com>
2025-10-22 19:43:37 +0530

    Initial commit for agricart-eureka-service

---
+++ src/test/java/com/agricart/discovery/eureka_service/EurekaServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.agricart.discovery.eureka_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
